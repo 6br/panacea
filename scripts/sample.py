bc48273e5df9654360b879f5bcf041c83193c92d
6br <tfdvrt@gmail.com>
2020-05-02 23:42:17 +0900

    Add screenshot

--- scripts/sample.py
+++ scripts/sample.py
@@ -3,6 +3,9 @@ import sys
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 # File Name
 FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "image/screen.png")
@@ -12,6 +15,7 @@ FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "image/scree
 driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
 url = 'http://user:pass@0.0.0.0:8080/?preset=default'
 driver.get(url)
+driver.set_page_load_timeout(10)
 #time.sleep(5)
 
 # get width and height of the page
@@ -21,7 +25,9 @@ h = driver.execute_script("return document.body.scrollHeight;")
 # set window size
 driver.set_window_size(w,h)
 
-element = WebDriverWait(driver, 10).until(lambda x: x.find_element_by_id("contentsLoaded"))
+time.sleep(2)
+
+element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'contentsLoaded')))
 
 time.sleep(2)
 
