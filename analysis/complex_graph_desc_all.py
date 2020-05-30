91cde1a211c55d93619d20bfc04307529f9fc3d4
6br <tfdvrt@gmail.com>
2020-05-30 13:57:20 +0900

    Change category

--- analysis/complex_graph_desc_all.py
+++ analysis/complex_graph_desc_all.py
@@ -55,7 +55,7 @@ def add_company(name, node_type):
 
     # Add category edge:
     if node_type[1:] in category_hash:
-        edge = [str(company_latest_id), "->", str(category_hash[node_type[1:]]), ":categorized_as", "label:" + "\"" + str(node_type).replace('"', '\\"') + "\""]
+        edge = [str(company_latest_id), "->", str(meta_hash[category_hash[node_type[1:]]]), ":categorized_as", "label:" + "\"" + str(node_type[1:]).replace('"', '\\"') + "\""]
         print("\t".join(edge))
     return company_hash[name]
 
